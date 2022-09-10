48dac9bb7c5c38da99fb45bb12eb3528f7459dc0
Karim Al-Atrash <karimalatrash@gmail.com>
2022-09-10 12:57:21 -0400

    adding ece alias

--- .termconfig/env.sh
+++ .termconfig/env.sh
@@ -11,6 +11,7 @@ eval "$(jump shell)"
 alias cppcompile='c++ -std=c++11 -stdlib=libc++'
 alias g='git'
 alias subl="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'"
+alias ecessh="ssh kalatras@eceubuntu.uwaterloo.ca -Y"
 
 # Add other sources
 source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
