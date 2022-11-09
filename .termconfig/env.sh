68c5f2b25c73ddb17157eac2e7edde695e5e798f
Karim Al-Atrash <karimalatrash@gmail.com>
2022-11-09 11:46:07 -0500

    adding new updates

--- .termconfig/env.sh
+++ .termconfig/env.sh
@@ -12,6 +12,8 @@ alias cppcompile='c++ -std=c++11 -stdlib=libc++'
 alias g='git'
 alias subl="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'"
 alias ecessh="ssh kalatras@eceubuntu.uwaterloo.ca -Y"
-
+alias mc-ssh="ssh opc@140.238.130.25"
+alias ecessh-j="ssh kalatras@eceterm.uwaterloo.ca kalatras@eceubuntu.uwaterloo.ca -Y"
+alias etermssh="ssh kalatras@eceterm.uwaterloo.ca -Y"
 # Add other sources
 source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
